883f01bb379bf4c5e8b909c6fa7dec6a8e3b8556
Osama <osama3akre@gmail.com>
2021-11-05 21:23:09 +0300

    some changes

---
+++ node.js
@@ -0,0 +1,10 @@
+//module is has an Object on it called exports
+
+module.exports = {};
+
+const foo = () => null;
+
+// equvalent to export default
+module.exports = foo;
+
+module.exports.bar = 'akre'; 
